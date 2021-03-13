f3f44b30700cde407a91dbd116505bf04cddff4e
unknown <abid.neduet@gmail.com>
2021-03-13 14:44:03 +0500

    my own prototype

--- Week 10/myPrototype.js
+++ Week 10/myPrototype.js
@@ -2,8 +2,19 @@ let array = [2,324,43,2,1,45,7,8,9];
 
 function Multiply(array){
     this.array = array;
+    // console.log(this.array)
 }
 
 Multiply.prototype.mul = (num)=>{
-    a
+    console.log(num)
+    console.log(array)
+    var newArray = array.map(myFunc)
+    function myFunc(item){
+        return num * item;
+    }
+    return newArray
+
 }
+
+var a = new Multiply(array)
+console.log(a.mul(9))
