f5685717f5abb7e7977d59f39b8dea2702e20721
unknown <abid.neduet@gmail.com>
2021-03-18 01:16:00 +0500

    redirect

--- Expense Management System/auth-pages/redirect.js
+++ Expense Management System/auth-pages/redirect.js
@@ -9,10 +9,44 @@ let redirect = (value) => {
 
 let getData = (event) => {
     event.preventDefault();
-    data = {
-        username : document.getElementById("username").value,
-        password : document.getElementById("password1").value,
-        email : document.getElementById("email").value
+    // swal("Good job!", "You clicked the button!", "success");
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password1").value
+    const email = document.getElementById("email").value
+    if(!username || !password || !email){
+        alert("Please enter in all fields")
     }
-    console.log(data)
+    const userData = {
+        email,
+        password,
+        username
+      }
+
+    let users = localStorage.getItem('AllUsers');
+    if(!)
+    users = [JSON.parse(users)];
+    users.push(userData);
+
+
+    userData = JSON.stringify(users);
+    localStorage.setItem('AllUsers', users)
+
+
+    // console.log(data)
+    // users = JSON.parse(users);
+    // users.push(userData);
+    // data = JSON.stringify(users)
+
+    // console.log(data)
+
+    // console.log('users : ', users)
+    // users = JSON.parse(users)
+  
+    // users.push(userData)
+  
+    // console.log('users :', users)
+  
+    // let stringUsersData = JSON.stringify(users)
+  
+ 
 }
