fbec0aa9e4df00069b3b202ea71a984aa9385542
unknown <abid.neduet@gmail.com>
2021-03-23 01:46:38 +0500

    config

---
+++ Expense Management System/expense-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+apiKey: "AIzaSyDLiSyfbkbC_RNi4RoOLE7BfmsNfyifvRc",
+authDomain: "expense-management-6801c.firebaseapp.com",
+projectId: "expense-management-6801c",
+storageBucket: "expense-management-6801c.appspot.com",
+messagingSenderId: "297903293836",
+appId: "1:297903293836:web:d75a62254c56548a68c2a9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
