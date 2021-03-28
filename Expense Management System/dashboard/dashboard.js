b522d0edc07af335ecd023707f8756ad3dc7b279
unknown <abid.neduet@gmail.com>
2021-03-28 20:43:11 +0500

    dashboard

--- Expense Management System/dashboard/dashboard.js
+++ Expense Management System/dashboard/dashboard.js
@@ -1,1 +1,14 @@
-// 
+
+
+db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+    console.log("wah   ")
+})
+.catch((error) => {
+    console.error("Error adding document: ", error);
+});
