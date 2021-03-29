47c996838f369049b648ca8797db4fd070283fd3
unknown <abid.neduet@gmail.com>
2021-03-30 00:24:22 +0500

    dashboard

--- Expense Management System/dashboard/dashboard.js
+++ Expense Management System/dashboard/dashboard.js
@@ -12,3 +12,76 @@
 // .catch((error) => {
 //     console.error("Error adding document: ", error);
 // });
+
+
+// db.collection("users").get().then((querySnapshot) => {
+//     querySnapshot.docs.forEach((doc) => {
+//         console.log(doc.data());
+//     });
+// });
+
+
+var uid;
+
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        uid = user.uid;
+        console.log("Current user is ", uid)
+
+        db.collection("expenses").get().then((querySnapshot) => {
+            querySnapshot.docs.forEach((doc) => {
+                // console.log(doc.data().uid);
+                if (doc.data().uid == uid) {
+                    console.log("category is :", doc.data().category)
+                    console.log("Expense is :", doc.data().expense)
+                }
+            });
+        });
+
+        // ...
+    } else {
+        // User is signed out
+        // ...
+    }
+})
+
+
+
+
+let expense = (e) => {
+    e.preventDefault();
+    const expense = document.getElementById("expense").value
+    const category = document.getElementById("category").value
+
+
+
+
+
+    db.collection("expenses").add({
+        uid: uid,
+        expense: expense,
+        date: Date.now(),
+        category: category
+    })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            console.log("wah")
+            window.location.href = "dashboard.html"
+
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+}
+
+
+
+
+
+
+
+
+
